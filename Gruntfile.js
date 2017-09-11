1acc29930b5e721a8521788471bf245e2e9f506d
andre487 <andre487@yandex-team.ru>
2017-09-11 22:53:02 +0300

    Fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,8 @@ module.exports = function (grunt) {
             main: {
                 files: [
                     {expand: true, cwd: 'src/root_files', src: '**', dest: 'build', filter: 'isFile'},
-                    {expand: true, cwd: 'src/assets', src: '**', dest: 'build/assets', filter: 'isFile'}
+                    {expand: true, cwd: 'src/assets', src: '**', dest: 'build/assets', filter: 'isFile'},
+                    {expand: true, cwd: 'src/assets', src: 'b888ae721f40.html', dest: 'build'}
                 ]
             }
         }
