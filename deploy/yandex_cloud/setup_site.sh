f5310d64d30eec76fc70435c25b98728502c73cf
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-04-25 23:02:41 +0300

    Implement YC runtime

---
+++ deploy/yandex_cloud/setup_site.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cur_dir="$(cd "$(dirname "$0")" && pwd)"
+source "$cur_dir/common.sh"
+
+ip_addr="$(get_prod_machine)"
+inventory_file="$TMPDIR/$MACHINE_NAME-inventory"
+echo "$ip_addr ansible_ssh_private_key_file=$ID_RSA ansible_ssh_user=$HOST_USER host_user=$HOST_USER" >"$inventory_file"
+
+#cd "$PROJECT_DIR"
+#npm ci
+#npm run build
+
+ansible-playbook -i "$inventory_file" "$cur_dir/setup.yml"
