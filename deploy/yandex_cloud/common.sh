2377c0126a0a5b7064803e6a72a723e923a09ae0
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-04-26 00:44:18 +0300

    Implement deploy to YC

--- deploy/yandex_cloud/common.sh
+++ deploy/yandex_cloud/common.sh
@@ -11,7 +11,9 @@ export DOCKER_IMAGE=nginx:1.19.10-alpine
 
 export LOCKBOX_SECRET_URL=https://payload.lockbox.api.cloud.yandex.net/lockbox/v1/secrets
 export SSH_KEY_SECRET=e6q6vfo2vo565h0cb0nq
+export DEPLOY_USER_SECRET=e6qg4280406q9aj6uvkv
 
+export GH_REPO="andre487/andre.life"
 export ID_RSA="$HOME/.ssh/id_rsa_cloud"
 export HOST_USER=yc-user
 
@@ -27,6 +29,17 @@ get_main_user_public_key() {
     fi
 
     echo "$secret_file"
+}
+
+get_deploy_user_data() {
+    local secret_file
+    local payload
+
+    payload="$(curl -s --fail -H "Authorization: Bearer $IAM_TOKEN" "$LOCKBOX_SECRET_URL/$DEPLOY_USER_SECRET/payload")"
+    jq <<< "$payload" -r '.entries | map(select(.key == "id_rsa")) | .[0].textValue' >"$SECRET_DIR/deploy_user_id_rsa"
+    jq <<< "$payload" -r '.entries | map(select(.key == "id_rsa.pub")) | .[0].textValue' >"$SECRET_DIR/deploy_user_id_rsa.pub"
+    jq <<< "$payload" -r '.entries | map(select(.key == "user_name")) | .[0].textValue' >"$SECRET_DIR/deploy_user_name"
+    jq <<< "$payload" -r '.entries | map(select(.key == "deploy_host")) | .[0].textValue' >"$SECRET_DIR/deploy_host"
 }
 
 get_prod_machine() {
--- deploy/yandex_cloud/setup_site.sh
+++ deploy/yandex_cloud/setup_site.sh
@@ -4,12 +4,25 @@ set -euo pipefail
 cur_dir="$(cd "$(dirname "$0")" && pwd)"
 source "$cur_dir/common.sh"
 
+if ! which gh >/dev/null; then
+    echo "There is no GitHub cli. Please install it: brew install gh"
+    exit 1
+fi
+
+get_deploy_user_data
+
+gh secret set DEPLOY_HOST --repos "$GH_REPO" --body "$(cat "$SECRET_DIR/deploy_host")"
+gh secret set DEPLOY_KEY --repos "$GH_REPO" --body "$(cat "$SECRET_DIR/deploy_user_id_rsa")"
+gh secret set DEPLOY_USER --repos "$GH_REPO" --body "$(cat "$SECRET_DIR/deploy_user_name")"
+
 ip_addr="$(get_prod_machine)"
 inventory_file="$TMPDIR/$MACHINE_NAME-inventory"
-echo "$ip_addr ansible_ssh_private_key_file=$ID_RSA ansible_ssh_user=$HOST_USER host_user=$HOST_USER" >"$inventory_file"
+echo "$ip_addr ansible_ssh_private_key_file=$ID_RSA ansible_ssh_user=$HOST_USER host_user=$HOST_USER secret_dir=$SECRET_DIR" >"$inventory_file"
 
-#cd "$PROJECT_DIR"
-#npm ci
-#npm run build
+cd "$PROJECT_DIR"
+if [[ ! -d node_modules ]]; then
+    npm ci
+fi
+npm run build
 
 ansible-playbook -i "$inventory_file" "$cur_dir/setup.yml"
